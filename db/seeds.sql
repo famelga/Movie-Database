940bdb87f7646c0fb7ba6f93fdef5dd4af7c1970
Fayven <fayven.amelga@gmail.com>
2023-02-01 16:18:24 -0500

    Add inserts

---
+++ db/seeds.sql
@@ -0,0 +1,13 @@
+INSERT INTO movies (movie_name)
+VALUES ("Legally Blonde"),
+        ("Avatar Two: Way of Water"),
+        ("The Dark Knight"),
+        ("Clueless"),
+        ("Inception");
+
+INSERT INTO reviews (review)
+VALUES ("great film! The bend and snap changed my life."),
+        ("One of the best visually appealing movies. The visuals are insane!"),
+        ("Love the true horror brought on by The Joker!"),
+        ("So quoteable, ugh, AS IF!"),
+        ("He's still dreaming...");
